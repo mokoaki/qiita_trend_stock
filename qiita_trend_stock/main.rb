461812931c28973a134d6e7b2ceff70b5d7175b0
mokoaki <mokoriso@gmail.com>
2018-03-25 14:47:57 +0900

    Refactoring main.rb

--- qiita_trend_stock/main.rb
+++ qiita_trend_stock/main.rb
@@ -20,28 +20,48 @@ module QiitaTrendStock
     def start(qiita_access_token:, stocked_item_uuid:, stock_keep_deadline:)
       qiita_client = Qiita::Client.new(access_token: qiita_access_token)
 
-      trend_items = TrendItems.new
+      trend_items     = trend_items_process(qiita_client, stock_keep_deadline)
+      stocked_items   = stocked_items_process(qiita_client, stocked_item_uuid)
+      new_trend_items = new_trend_items_process(qiita_client, trend_items, stocked_items)
+                        new_stocked_items_process(qiita_client, new_trend_items, stocked_items, stocked_item_uuid, stock_keep_deadline)
+
+      output_log(qiita_client, new_trend_items)
+    end
+
+    def trend_items_process(qiita_client, stock_keep_deadline)
       deadline_date = stock_keep_deadline.strftime('%F')
+
+      trend_items = TrendItems.new
       trend_items.fetch(qiita_client, "           stocks:>15 created:>#{deadline_date}")
       trend_items.fetch(qiita_client, "tag:Git    stocks:>1  created:>#{deadline_date}")
       trend_items.fetch(qiita_client, "tag:Rails  stocks:>1  created:>#{deadline_date}")
       trend_items.fetch(qiita_client, "tag:GitHub stocks:>1  created:>#{deadline_date}")
       trend_items.fetch(qiita_client, "tag:Ruby   stocks:>1  created:>#{deadline_date}")
 
+      trend_items
+    end
+
+    def stocked_items_process(qiita_client, stocked_item_uuid)
       stocked_items = StockedItems.new
       stocked_items.fetch(qiita_client, stocked_item_uuid)
 
+      stocked_items
+    end
+
+    def new_trend_items_process(qiita_client, trend_items, stocked_items)
       new_trend_items = NewTrendItems.new
       new_trend_items.find(trend_items, stocked_items)
       new_trend_items.items_stock!(qiita_client)
 
+      new_trend_items
+    end
+
+    def new_stocked_items_process(qiita_client, new_trend_items, stocked_items, stocked_item_uuid, stock_keep_deadline)
       new_stocked_items = NewStockedItems.new
       new_stocked_items.concat(new_trend_items)
       new_stocked_items.concat(stocked_items)
       new_stocked_items.delete_old_items!(stock_keep_deadline)
       new_stocked_items.store_stocked_uuids!(qiita_client, stocked_item_uuid)
-
-      output_log(qiita_client, new_trend_items)
     end
 
     def output_log(qiita_client, new_trend_items)
