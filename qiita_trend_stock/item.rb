492ea78fe6b835f3639631b2f98d94abf555aeb6
mokoaki <mokoriso@gmail.com>
2018-04-03 15:38:33 +0900

    Fix item time validate methods

--- qiita_trend_stock/item.rb
+++ qiita_trend_stock/item.rb
@@ -20,25 +20,22 @@ module QiitaTrendStock
 
     def validate_uuid(uuid)
       result = uuid.to_s.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '')
-      raise ArgumentError, uuid if result.blank?
+      raise(ArgumentError, uuid) if result.blank?
       result
     end
 
     def validate_time(time)
       return time if time.instance_of?(ActiveSupport::TimeWithZone)
 
-      parsed_time = time_parse(time)
-      return parsed_time if parsed_time.instance_of?(ActiveSupport::TimeWithZone)
-
-      # 何か変な事になっても、とりあえず今の日時を返す
-      # この日付はストックした履歴を確認したいだけで、特に重要ではないからである
-      Time.zone.now
-    end
-
-    def time_parse(time)
-      Time.zone.parse(time)
-    rescue StandardError
-      nil
+      begin
+        parsed_time = Time.zone.parse(time.to_s)
+        raise(ArgumentError, time) unless parsed_time.instance_of?(ActiveSupport::TimeWithZone)
+        parsed_time
+      rescue ArgumentError
+        # 何か変な事になっても、とりあえず今の日時を返す
+        # この日付はストックした履歴を確認したいだけで、特に重要ではないからである
+        Time.zone.now
+      end
     end
   end
 end
