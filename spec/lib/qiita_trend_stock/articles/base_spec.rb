7e5feee6b49b8cd75359d78c776930d48679fb20
mokoaki <mokoriso@gmail.com>
2019-03-04 00:24:48 +0900

    articles_base_spec 直し

--- spec/lib/qiita_trend_stock/articles/base_spec.rb
+++ spec/lib/qiita_trend_stock/articles/base_spec.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-# require 'spec_helper'
-# require_relative '../../../../lib/qiita_trend_stock/articles/base'
+require 'spec_helper'
+require_relative '../../../../lib/qiita_trend_stock/articles/base'
 
-# module QiitaTrendStock
-#   RSpec.describe Articles do
-#     let(:articles) { Articles.new }
+RSpec.describe QiitaTrendStock::Articles do
+  let(:articles) do
+    described_class.new
+  end
 
-#     context '#initialize' do
-#       it 'コンストラクタは[@fetched_articles,@stocked_articles]を空配列で初期化する' do
-#         expect(articles.instance_variable_get(:@fetched_articles)).to eq([])
-#         expect(articles.instance_variable_get(:@stocked_articles)).to eq([])
-#       end
-#     end
-#   end
-# end
+  context '#initialize' do
+    it 'コンストラクタは[@fetched_articles,@stocked_articles]を空配列で初期化する' do
+      expect(articles.instance_variable_get(:@fetched_articles)).to eq([])
+      expect(articles.instance_variable_get(:@stocked_articles)).to eq([])
+    end
+  end
+end
