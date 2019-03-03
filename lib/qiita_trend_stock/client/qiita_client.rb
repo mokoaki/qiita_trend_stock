9f6adfa5cf7fccd06fc9c99319b77ff4bd3e03a4
mokoaki <mokoriso@gmail.com>
2019-03-03 21:57:33 +0900

    よくわからなくなりました　いろいろいじりました　ごめんなさい

--- lib/qiita_trend_stock/client/qiita_client.rb
+++ lib/qiita_trend_stock/client/qiita_client.rb
@@ -21,56 +21,56 @@ module QiitaTrendStock
       def user_status
         EncapsulationQiitaClient.user_status
       end
+    end
 
-      # ・・の実装
-      module EncapsulationQiitaClient
-        module_function
+    # ・・の実装
+    module EncapsulationQiitaClient
+      module_function
 
-        def query_articles(page, query)
-          client.list_items(page: page, per_page: 100, query: query)
-        end
+      def query_articles(page, query)
+        client.list_items(page: page, per_page: 100, query: query)
+      end
 
-        def stock_item(uuid)
-          responce_body = client.stock_item(uuid).body
+      def stock_item(uuid)
+        responce_body = client.stock_item(uuid).body
 
-          # stock成功時はbodyはnilらしい
-          return true if responce_body.nil?
+        # stock成功時はbodyはnilらしい
+        return true if responce_body.nil?
 
-          responce_message = responce_message(responce_body)
+        responce_message = responce_message(responce_body)
 
-          # stock済みは「stockしなかった」のfalseを返す
-          return false if responce_message == 'Already stocked'
+        # stock済みは問題なし
+        # 「stockしなかった」のfalseを返す
+        return false if responce_message == 'Already stocked'
 
-          # エラー時は適当に出力して正常に終了したかの如く処理を続ける
-          p '=' * 100
-          p "stock_item(#{uuid}) Error"
-          p responce_body
+        # エラー時は適当に出力
+        p '=' * 100
+        p responce_body, uuid: uuid
 
-          false
-        end
+        false
+      end
 
-        def responce_message(responce_body)
-          responce_body['message']
-        rescue StandardError
-          responce_body
-        end
+      def responce_message(responce_body)
+        responce_body['message']
+      rescue StandardError
+        responce_body
+      end
 
-        def user_status
-          auth_headers = client.get_authenticated_user.headers
-          allow_keys = %w[Rate-Limit Rate-Remaining Rate-Reset X-Runtime Date]
-          result_headers = auth_headers.slice(*allow_keys)
-          result_headers['DateJST'] =
-            Time.zone.parse(result_headers['Date']).to_s
-          result_headers
-        end
+      def user_status
+        auth_headers = client.get_authenticated_user.headers
+        allow_keys = %w[Rate-Limit Rate-Remaining Rate-Reset X-Runtime Date]
+        result_headers = auth_headers.slice(*allow_keys)
+        jst_string = Time.zone.parse(result_headers['Date']).to_s
+        result_headers['DateJST'] = jst_string
+        result_headers
+      end
 
-        def client
-          @client ||= ::Qiita::Client.new(access_token: access_token)
-        end
+      def client
+        @client ||= ::Qiita::Client.new(access_token: access_token)
+      end
 
-        def access_token
-          ENV['QIITA_ACCESS_TOKEN']
-        end
+      def access_token
+        ENV['QIITA_ACCESS_TOKEN']
       end
     end
   end
