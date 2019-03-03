e95e344a40bdf1ead215abba9ae240b01ec9b813
mokoaki <mokoriso@gmail.com>
2019-03-03 21:49:44 +0900

    よくわからなくなりました　いろいろいじりました　ごめんなさい

--- lib/qiita_trend_stock/client/qiita_client.rb
+++ lib/qiita_trend_stock/client/qiita_client.rb
@@ -59,7 +59,8 @@ module QiitaTrendStock
           auth_headers = client.get_authenticated_user.headers
           allow_keys = %w[Rate-Limit Rate-Remaining Rate-Reset X-Runtime Date]
           result_headers = auth_headers.slice(*allow_keys)
-          result_headers['DateJST'] = Time.zone.parse(result_headers['Date']).to_s
+          result_headers['DateJST'] =
+            Time.zone.parse(result_headers['Date']).to_s
           result_headers
         end
 
