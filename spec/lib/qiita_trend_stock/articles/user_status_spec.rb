c418ba1bb154d0219833aa767c406667ed9f65a1
mokoaki <mokoriso@gmail.com>
2019-03-04 18:22:33 +0900

    Add articles/user_status_spec

--- spec/lib/qiita_trend_stock/articles/user_status_spec.rb
+++ spec/lib/qiita_trend_stock/articles/user_status_spec.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
-# require 'spec_helper'
-# require_relative '../../../../lib/qiita_trend_stock/articles/user_status'
+require 'spec_helper'
+require_relative '../../../../lib/qiita_trend_stock/articles/user_status'
 
-# RSpec.describe QiitaTrendStock::UserStatus do
-#   let(:articles) do
-#     QiitaTrendStock::Articles.new
-#   end
-# end
+RSpec.describe QiitaTrendStock::UserStatus do
+  let(:articles_class) do
+    Class.new { include QiitaTrendStock::UserStatus }
+  end
+
+  let(:articles) { articles_class.new }
+
+  let(:test_interface) { double(:test_interface) }
+
+  context '#user_status' do
+    example 'Interface.user_statusに委譲する' do
+      stub_const('Interface', test_interface)
+      expect(test_interface).to receive(:user_status)
+      articles.user_status
+    end
+  end
+end
