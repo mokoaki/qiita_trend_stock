e8ae7107fa82a248ea7027562a9edbc0c59e5716
mokoaki <mokoriso@gmail.com>
2019-03-04 16:00:42 +0900

    Add なんか無かったファイル

---
+++ spec/lib/qiita_trend_stock/articles/user_status_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# require 'spec_helper'
+# require_relative '../../../../lib/qiita_trend_stock/articles/user_status'
+
+# RSpec.describe QiitaTrendStock::UserStatus do
+#   let(:articles) do
+#     QiitaTrendStock::Articles.new
+#   end
+# end
